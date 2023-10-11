6381d95559cb9a73ca0be484ad1b94e0a5771327
Leons Aleksandrovs <skrazzo@proton.me>
2023-10-11 14:44:32 +0300

    Made clean version of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className='flex flex-col items-center container mx-auto max-w-2xl shadow  rounded border-blue-400 border p-4 my-5'>
+            <span className='text-xl '>🩵 Thank you for cloning clean version of me 🩵</span>
+        </div>
+    )
 }
-
-export default App;
